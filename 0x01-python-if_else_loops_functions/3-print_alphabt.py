65f97c15690954ca0989135374d3b6d09c748428
Tommy okoyo <okoyotommy@gmail.com>
2022-11-29 08:38:00 -0500

    print alphabet 2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i != "q" or i != "e":
-        print("{}".format(chr(i)), end="")
+    if i == "q" and i == "e":
+        continue
+    else:
+        print("{}".format(chr(i)), end = "")
