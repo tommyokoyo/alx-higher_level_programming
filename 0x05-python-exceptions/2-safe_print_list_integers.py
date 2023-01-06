49faf015546e1ba0aa9a1f9f03c5160c62d51e7c
Tommy_okoyo <okoyo@TommyMac.local>
2023-01-07 01:07:40 +0300

    print and count intergers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    lst = 0
+    for a in range(x):
+        try:
+            if type(a) == int:
+                print("{:d}".format(my_list[a]), end="")
+                lst += 1
+            else:
+                continue
+        except (TypeError, ValueError):
+            continue
+    
+    print("")
+    return lst
