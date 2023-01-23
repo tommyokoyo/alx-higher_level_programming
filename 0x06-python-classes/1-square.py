62b2c2992f46bf2fb7f82dc9ef627af92408a62c
Tommy_okoyo <okoyo@TommyMac.local>
2023-01-23 21:52:59 +0300

    1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,10 +8,11 @@ class Square:
     """
     Class with private instance variable
     """
+
+
     def __init__(self, size=0):
         """
         Args:
             size: size of square
         """
-
-        self._size = size
+        self.__size = size
