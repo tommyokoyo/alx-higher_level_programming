64145a5091af74a7c4145455948add4ca251d811
Tommy_okoyo <okoyo@TommyMac.local>
2023-01-23 22:01:28 +0300

    size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+square class defination
+"""
+
+
+class Square:
+    """
+    Class with private instance variable
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of square
+        """
+        if type(size) is not int:
+            raise TypeError("Size must be an interger")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
