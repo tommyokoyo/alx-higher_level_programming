5d77659cd2edb59450fcf5df3b6e7198dbc86c7b
Tommy_okoyo <okoyo@TommyMac.local>
2023-01-27 00:46:59 +0300

    printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+square class defination
+"""
+
+
+class Square:
+    """
+    Class with private instance variable
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of square
+        """
+        if type(size) is not int:
+            raise TypeError("Size must be an interger")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size)**2
+
+    @property
+    def size(self):
+        """size: size of the square
+        setter validating size is int and >= 0
+
+        Raise:
+             TypeError and ValueError
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size: size of the square
+        setter validating size is int and >= 0
+
+        Raise:
+             TypeError and ValueError
+        """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """
+            prints in stdout the square with the character #
+        """
+        for i in range(self.size):
+            for j in range(self.size):
+                   print("#", end="")
+            print("\n")
