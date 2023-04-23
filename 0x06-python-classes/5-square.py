af0d6df530741dbcf7d8d3fa3a564078f4d21e00
tommyokoyo <okoyotommy@gmail.com>
2023-04-23 15:26:00 +0300

    Correction

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -14,12 +14,8 @@ class Square:
         Args:
             size: size of square
         """
-        if type(size) is not int:
-            raise TypeError("Size must be an interger")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        
+        self.__size = size
 
     def area(self):
         return (self.__size)**2
