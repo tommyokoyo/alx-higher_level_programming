f57dcf0fbdf5dc45f26fd49d53b843290face860
tommyokoyo <okoyotommy@gmail.com>
2023-05-13 13:32:08 -0400

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 
 // Scripts that print C on output x number of times
 if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
   for (let n = 1; n <= parseInt(process.argv[2]); n++) {
     console.log('C is fun”');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
