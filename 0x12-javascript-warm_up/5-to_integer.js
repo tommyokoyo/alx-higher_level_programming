5cee92e749a783b8c515eb29edbcd311aeb1b0c0
tommyokoyo <okoyotommy@gmail.com>
2023-05-13 12:54:16 -0400

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// checks if arguments can be converted into an interger
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
