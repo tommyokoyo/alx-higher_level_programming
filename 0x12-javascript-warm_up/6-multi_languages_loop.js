a5bf2732c82b0fe0b6d77cc9666eba5059b2e233
tommyokoyo <okoyotommy@gmail.com>
2023-05-13 13:08:35 -0400

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// print 3 lines by use of an array and loop
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let n = 0; n < 3; n++) {
+  console.log(list[n]);
+}
