9411fa95eb3cf25e5406e7fceef4d54c40bbeb2d
tommyokoyo <okoyotommy@gmail.com>
2023-05-14 16:16:41 -0400

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 
 // Prints a square
 if (isNaN(process.argv[2])) {
-    console.log('Missing size');
-} else { 
-    for (let n = 0; n < parseInt(process.argv[2]); n++) {
-        console.log('X'.repeat(parseInt(process.argv[2])));
-    }
+  console.log('Missing size');
+} else {
+  for (let n = 0; n < parseInt(process.argv[2]); n++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+  }
 }
