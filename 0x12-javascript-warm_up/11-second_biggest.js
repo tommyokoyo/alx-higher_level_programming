cc1f0044de38345b1ffa2899f34889b4fecf2068
tommyokoyo <okoyotommy@gmail.com>
2023-05-14 16:56:31 -0400

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 // Parses for the second biggest interger in a list
 if (process.argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
   const argList = process.argv.sort();
   console.log(argList.reverse()[1]);
