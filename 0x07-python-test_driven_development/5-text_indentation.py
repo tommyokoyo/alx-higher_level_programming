c7b1f4c42ec87cf7d4ce26f61b75f7d8f1f50dff
tommyokoyo <okoyotommy@gmail.com>
2023-05-28 16:34:37 -0400

    indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    function that applies indentation
+"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after ., ?, and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    delimiter = ['.', '?', ':']
+    s = ""
+
+    for c in text:
+        s += c
+        if c in delimiter:
+            print(s.strip())
+            print()
+            s = ""
+    print(s.strip(), end='')
+    if text is "\n":
+        print()
