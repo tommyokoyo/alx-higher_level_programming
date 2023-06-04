d0a0f83b3b33b986a6de053d0c18e2e2e91530d4
tommyokoyo <okoyotommy@gmail.com>
2023-06-04 14:56:47 -0400

    added instance method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Creates a class Rectangle that defines a rectangle
+// Initialize the widht and height
+// instance method print to print rectangle
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
