7ba17eee9f4be428dc4d5a2a81eb6cbd7953ecd3
tommyokoyo <okoyotommy@gmail.com>
2023-06-04 15:11:13 -0400

    changed name

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const rectangle = require('./4-rectangle');
+const Rectangle = require('./4-rectangle');
 
 /*
  * Square class that defines a square and
  * Inherits from rectangle class of 4-rectangle
  */
-module.exports = class square extends rectangle {
+module.exports = class square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
