de4f744afab64a244defe1a93d11ef7578a66fe5
tommyokoyo <okoyotommy@gmail.com>
2023-06-04 14:44:58 -0400

    added constuctor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Creates a class Rectangle that defines a rectangle
+// Initialize the widht and height
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
