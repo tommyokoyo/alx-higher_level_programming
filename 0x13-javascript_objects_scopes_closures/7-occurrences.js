143178ac0b32fe5967c3725ad04b24dadc136c88
tommyokoyo <okoyotommy@gmail.com>
2023-06-04 15:33:58 -0400

    count occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+/**
+ * nbOccurences - Counts the number of occurences in a list
+ * @list: Array of data
+ * @searchElement: Element being counted
+ *
+ * Return: number of occurences
+ */
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  for (let n = 0; n < list.length; ++n) {
+    if (list[1] === searchElement) {
+      ++count;
+    }
+  }
+  return count;
+};
